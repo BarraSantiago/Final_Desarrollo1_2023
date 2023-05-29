4d0305d977970d6ac6722395fda562b21b9a08d2
Santi <barrasanti@gmail.com>
2023-05-29 11:47:30 -0300

    added projectile

--- Final_Desarrollo1_2023/Entity/Entity.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-namespace Entity
-{
-    class Entity
-    {
-    public:
-    };
-}
---
+++ Final_Desarrollo1_2023/Entity/Objects/Projectile.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "..\DefaultEntity.h"
+
+namespace Entity
+{
+    class Unit;
+}
+
+namespace Objects
+{
+    class Projectile : public Entity::DefaultEntity
+    {
+    public:
+        Projectile();
+        Projectile(float speed, float damage, Rectangle body,Vector2 direction, Entity::Team team);
+        Projectile(Vector2 direction, Entity::Team team);
+        ~Projectile() override;
+        
+        Rectangle GetBody() override;
+
+        void Move();
+        void SetTarget(Entity::Unit* target);
+        void DrawBody() override;
+        void Collide();
+
+    private:
+        float speed;
+        float damage;
+
+        bool hasTarget;
+        bool isAlive;
+
+        Vector2 direction{};
+        Entity::Team team;
+        Entity::Unit* target;
+        //float
+    };
+}
