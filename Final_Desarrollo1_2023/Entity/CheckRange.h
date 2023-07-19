3d5f41845388ed7c271d26d18df1688025594bb9
BarraSantiago <56093356+BarraSantiago@users.noreply.github.com>
2023-07-19 19:18:46 -0300

    Remade proyect, old proyect clean up

--- Final_Desarrollo1_2023/Entity/CheckRange.h
+++
@@ -1,8 +0,0 @@
-﻿#pragma once
-#include "Unit.h"
-
-namespace Check
-{
-    bool InRange(Rectangle body, Rectangle targetBody, float range);
-    bool SameTeam(Entity::Team team, Entity::Team targetTeam);
-}
