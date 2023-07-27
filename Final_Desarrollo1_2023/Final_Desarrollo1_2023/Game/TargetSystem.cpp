fc2fe09e0ef67b532d0098121d771614e96e877f
BarraSantiago <56093356+BarraSantiago@users.noreply.github.com>
2023-07-26 23:57:28 -0300

    clean up

--- Final_Desarrollo1_2023/Final_Desarrollo1_2023/Game/TargetSystem.cpp
+++ Final_Desarrollo1_2023/Final_Desarrollo1_2023/Game/TargetSystem.cpp
@@ -1,7 +1,7 @@
 #include "TargetSystem.h"
 
 #include <raylib.h>
-#include <raymath.h>
+#include <cmath>
 
 #include "GameManager.h"
 #include "../Entity/Unit.h"
@@ -9,20 +9,24 @@ using namespace Entity;
 
 namespace targeting
 {
+    float CalculateDistance(const Vector2& point1, const Vector2& point2) {
+        float dx = point2.x - point1.x;
+        float dy = point2.y - point1.y;
+        return std::sqrt(std::pow(dx, 2) + std::pow(dy, 2));
+    }
     Unit* GetTarget(Vector2 position, std::vector<Unit*> units)
     {
+        if(units.empty()) return nullptr;
+        
         Unit* newTarget = units[0];
         
-        for (Unit* target : units)
-        {
-            if(!target->IsAlive()) continue;
-            
-            Vector2 targetPos = {newTarget->GetBody().x, newTarget->GetBody().y};
-            Vector2 playerPos = {target->GetBody().x, target->GetBody().y};
+        float nearestDistance = CalculateDistance({newTarget->body.x, newTarget->body.y}, position);
 
-            if (Vector2Distance(playerPos, position) < Vector2Distance(targetPos, position))
-            {
-                newTarget = target;
+        for (size_t i = 1; i < units.size(); ++i) {
+            float distance = CalculateDistance({units[i]->body.x, units[i]->body.y}, position);
+            if (distance < nearestDistance) {
+                nearestDistance = distance;
+                newTarget = units[i];
             }
         }
         return newTarget;
